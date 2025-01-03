ad4f994975432b0f3c813d1eac3af7db752ee19b
24-python <24_python@mail.ru>
2025-01-03 20:01:12 +0300

    2

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 from flask import Flask, render_template
 
+# Создание приложения Flask
 app = Flask(__name__)
 
+# Маршрут для главной страницы
 @app.route('/')
 def index():
     return render_template('index.html')
 
+# Маршрут для страницы блога
 @app.route('/blog/')
 def blog():
     return render_template('blog.html')
 
+# Маршрут для страницы контактов
 @app.route('/contacts/')
 def contacts():
     return render_template('contacts.html')
